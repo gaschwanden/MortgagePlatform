9e4db59a0fc462df2be671aaa37c959c42dba923
Yi Song (Ethan) <ethaninmel@gmail.com>
2018-04-27 12:13:44 +1000

    Add package nuxt

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -22,6 +22,7 @@ contract ERC721Metadata is ERC721Basic {
   function name() public view returns (string _name);
   function symbol() public view returns (string _symbol);
   function tokenURI(uint256 _tokenId) public view returns (string);
+  function asd();
 }
 
 
