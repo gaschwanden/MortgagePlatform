b13baf4120ddd9205c80f81720ff238f503d12f0
Yi Song (Ethan) <ethaninmel@gmail.com>
2018-04-30 11:26:03 +1000

    Setup Vue dev environment

---
+++ front-end/nuxt.config.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+    /*
+    ** Global CSS
+    */
+   css: [
+    'node_modules/element-ui/lib/theme-chalk/index.css'
+    ],
+    /*
+    ** Add element-ui in our app, see plugins/element-ui.js file
+    */
+    plugins: [
+      '@/plugins/element-ui'
+    ]
+  }
